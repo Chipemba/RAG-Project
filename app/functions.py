67946a4f0bf715d99ec78f644b1a60dc81653e44
Chipemba <chipemba15@gmail.com>
2025-10-18 19:31:00 -0400

    App using Ollama

--- app/functions.py
+++ app/functions.py
@@ -1,12 +1,14 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+
+from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.vectorstores import Chroma
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
-from pydantic import BaseModel, Field
+from langchain_core.pydantic_v1 import BaseModel, Field
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from tempfile import NamedTemporaryFile
 
 import base64
 import os
@@ -15,25 +17,40 @@ import uuid
 import pandas as pd
 import re
 import json
+import random
+import time
+import requests
+import streamlit as st
 
 
-"""
-    Gets the urls in the urls.txt and begins saving the pages as pdf.
-    Uses selenium to printscreen and save the PDFs into the pdf folder. 
-"""
-def getUrls():
+urls = [
+        "https://www.jdpower.com/motorcycles/shopping-guides/beginner-dirt-bike-for-adults",
+        "https://www.riderswestmag.com/motomayhem/article/your_first_dirt_bike_buy_the_reality_not_the_dream",
+        "https://www.frpmoto.com/blogs/dirt-bike-specifications/understanding-dirt-bike-specifications-a-beginners-guide",
+        "https://blog.amsoil.com/what-to-look-for-when-buying-a-used-dirt-bike/",
+        "https://ca.riskracing.com/blogs/news/what-do-i-need-to-know-before-buying-a-dirt-bike?srsltid=AfmBOoporUN6YY4RcrcyPV3MmkIRgy8OMe61-yxN3kj4GzyTzQbMaKfx&shpxid=a3a2c14b-3544-4bbe-98d2-b8dcb508d67f"
+        ]
 
-    with open('pdfData\articaleUrls.txt') as urlFile:
-        urls = urlFile.readlines()
 
-    return urls
 
-def saveUrlsAsPdfs(outputFolder="pdfData\pdfs"):
-    
-    urls = getUrls()
+def clean_filename(filename):
+    """
+    Remove "(number)" pattern from a filename 
+    (because this could cause error when used as collection name when creating Chroma database).
 
-    if not os.path.exists(outputFolder):
-        os.makedirs(outputFolder)
+    Parameters:
+        filename (str): The filename to clean
+
+    Returns:
+        str: The cleaned filename
+    """
+    # Regular expression to find "(number)" pattern
+    new_filename = re.sub(r'\s\(\d+\)', '', filename)
+    return new_filename
+
+
+
+def get_pdf_text(): 
 
     chrome_options = Options()
     chrome_options.add_argument('--headless')
@@ -49,45 +66,38 @@ def saveUrlsAsPdfs(outputFolder="pdfData\pdfs"):
     chrome_options.add_argument('--kiosk-printing')
 
     driver = webdriver.Chrome(options=chrome_options)
-
-    for i, url in enumerate(urls):
+    documents=[]
+    temps=[]
+    i=0
+    for url in urls: 
+        # random.choices(urls, k=5):
+        
         try:
+            
             driver.get(url)
-            result = driver.execute_cdp_cmd("Page.printToPDF", {
-                "landscape": False,
-                "printBackground": True
-            })
 
-            pdf_data = base64.b64decode(result['data'])
-            file_name = f"retrievedPDF_{i+1}.pdf"
-            with open(os.path.join(outputFolder, file_name), "wb") as f:
-                f.write(pdf_data)
-            print(f"Saved: {file_name}")
+            pdf = driver.execute_cdp_cmd("Page.printToPDF", {"landscape": False,"printBackground": True})
+            pdf_bytes = base64.b64decode(pdf['data'])
+
+            pdf_base64 = base64.b64encode(pdf_bytes).decode('utf-8')
+     
+            with tempfile.NamedTemporaryFile(suffix=".pdf", delete=False) as temp_pdf:
+                temp_pdf.write(pdf_bytes)
+                temp_pdf_path = temp_pdf.name   
+                loader = PyPDFLoader(temp_pdf_path )
+                documents.extend(loader.load())
+    
         except Exception as e:
             print(f"Failed to save {url}: {e}")
+        finally:
+            os.unlink(temp_pdf.name)
+        
+        i=i+1
 
     driver.quit()
-
-
-"""
-    Load all the PDF documents from the pdf folder into the documents parameter.
-    Parameters:
-        uploaded_file (file-like object): The uploaded PDF file to load
-    Returns:
-        list: A list of documents created from the uploaded PDF files
-"""
-def get_pdf_text(): 
-
-    saveUrlsAsPdfs()
-    
-    loader = DirectoryLoader('pdfData\pdfs', glob = "./*.pdf", loader_cls= PyPDFLoader )
-    documents = loader.load()
-
+   
     return documents
-    
-    # finally:
-    #     # Ensure the temporary file is deleted when we're done with it
-    #     os.unlink(temp_file.name)
+
 
 
 def split_document(documents, chunk_size, chunk_overlap):    
@@ -95,6 +105,7 @@ def split_document(documents, chunk_size, chunk_overlap):
     Function to split generic text into smaller chunks.
     chunk_size: The desired maximum size of each chunk (default: 400)
     chunk_overlap: The number of characters to overlap between consecutive chunks (default: 20).
+
     Returns:
         list: A list of smaller text chunks created from the generic text
     """
@@ -106,28 +117,36 @@ def split_document(documents, chunk_size, chunk_overlap):
     return text_splitter.split_documents(documents)
 
 
-def get_embedding_function():
+
+def get_embedding_function(api_key):
     """
     Return an OpenAIEmbeddings object, which is used to create vector embeddings from text.
     The embeddings model used is "text-embedding-ada-002" and the OpenAI API key is provided
     as an argument to the function.
+
     Parameters:
         api_key (str): The OpenAI API key to use when calling the OpenAI Embeddings API.
+
     Returns:
         OpenAIEmbeddings: An OpenAIEmbeddings object, which can be used to create vector embeddings from text.
     """
-    embeddings = OllamaEmbeddings(model="llama3.1",)
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-ada-002", openai_api_key=api_key
+    )
     return embeddings
+
 
 
 def create_vectorstore(chunks, embedding_function, file_name, vector_store_path="db"):
 
     """
     Create a vector store from a list of text chunks.
+
     :param chunks: A list of generic text chunks
     :param embedding_function: A function that takes a string and returns a vector
     :param file_name: The name of the file to associate with the vector store
     :param vector_store_path: The directory to store the vector store
+
     :return: A Chroma vector store object
     """
 
@@ -158,7 +177,9 @@ def create_vectorstore(chunks, embedding_function, file_name, vector_store_path=
     return vectorstore
 
 
-def create_vectorstore_from_texts(documents, file_name):
+
+
+def create_vectorstore_from_texts(documents, api_key, filename):
     
     # Step 2 split the documents  
     """
@@ -173,29 +194,17 @@ def create_vectorstore_from_texts(documents, file_name):
     docs = split_document(documents, chunk_size=1000, chunk_overlap=200)
     
     # Step 3 define embedding function
-    embedding_function = get_embedding_function()
+    embedding_function = get_embedding_function(api_key)
 
     # Step 4 create a vector store  
-    vectorstore = create_vectorstore(docs, embedding_function, file_name)
+    vectorstore = create_vectorstore(docs, embedding_function, filename)
     
     return vectorstore
 
-def clean_filename(filename):
-    """
-    Remove "(number)" pattern from a filename 
-    (because this could cause error when used as collection name when creating Chroma database).
 
-    Parameters:
-        filename (str): The filename to clean
 
-    Returns:
-        str: The cleaned filename
-    """
-    # Regular expression to find "(number)" pattern
-    new_filename = re.sub(r'\s\(\d+\)', '', filename)
-    return new_filename
 
-def load_vectorstore(file_name, vectorstore_path="db"):
+def load_vectorstore(file_name, api_key, vectorstore_path="db"):
 
     """
     Load a previously saved Chroma vector store from disk.
@@ -206,7 +215,7 @@ def load_vectorstore(file_name, vectorstore_path="db"):
     
     :return: A Chroma vector store object
     """
-    embedding_function = get_embedding_function()
+    embedding_function = get_embedding_function(api_key)
     return Chroma(persist_directory=vectorstore_path, 
                   embedding_function=embedding_function, 
                   collection_name=clean_filename(file_name))
@@ -215,7 +224,7 @@ def load_vectorstore(file_name, vectorstore_path="db"):
 PROMPT_TEMPLATE = """
 You are an chatbot assisting with answering frequently asked 
 question for a website that sales outdoor vehicles.
-Your main focus is only on dirt bikes.
+Your main focus is only on two wheel and four wheel motorcycle bikes.
 Use the following pieces of retrieved context to answer
 the question. If you don't know the answer, say that you
 don't know. DON'T MAKE UP ANYTHING.
@@ -228,27 +237,33 @@ class AnswerWithSources(BaseModel):
     """An answer to the question, with sources and reasoning."""
     answer: str = Field(description="Answer to question")
     sources: str = Field(description="Full direct text chunk from the context used to answer the question")
-    # date: str = Field(description="When was the article published")
     reasoning: str = Field(description="Explain the reasoning of the answer based on the sources")
     
 
 class ExtractedInfoWithSources(BaseModel):
     """Extracted information about the research article"""
     paper_title: AnswerWithSources
-    publication_date: AnswerWithSources
+    paper_summary: AnswerWithSources
+    publication_year: AnswerWithSources
     paper_authors: AnswerWithSources
+
+
 
 def format_docs(docs):
     """
     Format a list of Document objects into a single string.
+
     :param docs: A list of Document objects
+
     :return: A string containing the text of all the documents joined by two newlines
     """
     return "\n\n".join(doc.page_content for doc in docs)
 
 # retriever | format_docs passes the question through the retriever, generating Document objects, and then to format_docs to generate strings;
 # RunnablePassthrough() passes through the input question unchanged.
-def query_document(vectorstore, query):
+
+
+def query_document(vectorstore, query, api_key):
 
     """
     Query a vector store with a question and return a structured response.
@@ -259,7 +274,7 @@ def query_document(vectorstore, query):
 
     :return: A pandas DataFrame with three rows: 'answer', 'source', and 'reasoning'
     """
-    llm = ChatOllama(model="llama3.1", format=ExtractedInfoWithSources.model_json_schema(),)
+    llm = ChatOpenAI(model="gpt-4o-mini", api_key=api_key)
 
     retriever=vectorstore.as_retriever(search_type="similarity")
 
@@ -268,12 +283,11 @@ def query_document(vectorstore, query):
     rag_chain = (
             {"context": retriever | format_docs, "question": RunnablePassthrough()}
             | prompt_template
-            | llm
+            | llm.with_structured_output(ExtractedInfoWithSources, strict=True)
         )
 
-    response = rag_chain.invoke(query)
-    # validResponse = jobPostInfoWithSources.model_validate_json(response.content)
-    df = pd.DataFrame([json.loads(response.content)])
+    structured_response = rag_chain.invoke(query)
+    df = pd.DataFrame([structured_response.dict()])
 
     # Transforming into a table with two rows: 'answer' and 'source'
     answer_row = []
